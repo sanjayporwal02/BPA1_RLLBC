60de4b385796673c2f1e8b7a1e4076212b4d170e
sanjayporwal02 <sanjayporwal02@gmail.com>
2022-06-10 20:56:29 +0200

    Completed

--- assignment/task/ValueIterationAgent.py
+++ assignment/task/ValueIterationAgent.py
@@ -2,6 +2,7 @@ from agent import Agent
 import numpy as np
 
 # TASK 2
+
 class ValueIterationAgent(Agent):
 
     def __init__(self, mdp, discount=0.9, iterations=100):
@@ -18,8 +19,9 @@ class ValueIterationAgent(Agent):
         number_states = len(states)
         # *************
         #  TODO 2.1 a)
-        # self.V = ...
-
+        self.V = {s: 0 for s in states}
+        self.pi = {s: self.mdp.getPossibleActions(s)[-1] if self.mdp.getPossibleActions(s) else None for s in states}
+        
         # ************
 
         for i in range(iterations):
@@ -28,13 +30,23 @@ class ValueIterationAgent(Agent):
                 actions = self.mdp.getPossibleActions(s)
                 # **************
                 # TODO 2.1. b)
-                # if ...
-                #
-                # else: ...
-
+                if mdp.isTerminal(s) == True:
+                    newV[s] = 0.0
+                   
+                else: 
+                    v_pi = {a: 0 for a in actions}
+                    count=0
+                    for a in actions: # iterate through every possible action : where is the best value, i.e., a = argmax_a q(s,a)
+                    # update value estimate
+                        reward = self.mdp.getReward(s,a,None)
+                        successors = mdp.getTransitionStatesAndProbs(s,a)
+                        for next_state, prob in successors: #looping over all the possible transitions from state s
+                            v_pi[a] += prob*(reward + discount*self.V[next_state])
+                            count +=1
+                    newV[s] = max(v_pi.values())
+                   
                 # Update value function with new estimate
-                # self.V =
-
+                self.V[s] = newV[s]
                 # ***************
 
     def getValue(self, state):
@@ -44,7 +56,7 @@ class ValueIterationAgent(Agent):
         """
         # **********
         # TODO 2.2
-
+        return self.V[state]
         # **********
 
     def getQValue(self, state, action):
@@ -57,7 +69,12 @@ class ValueIterationAgent(Agent):
         """
         # ***********
         # TODO 2.3.
-
+        successors = self.mdp.getTransitionStatesAndProbs(state, action) #get all successor states and probabilities
+        reward = self.mdp.getReward(state,action,None) 
+        q_value = 0.0
+        for next_state, prob in successors: #looping over all the possible transitions from state s
+            q_value += prob*(reward + self.discount*self.V[next_state])
+        return q_value
         # **********
 
     def getPolicy(self, state):
@@ -71,10 +88,14 @@ class ValueIterationAgent(Agent):
             return None
 
         else:
-            pass #delete after implemeting below task
+            
         # **********
         # TODO 2.4
-
+            q_value = {a: 0 for a in actions}
+            for a in actions:
+                q_value[a] += self.getQValue(state,a)
+            self.pi[state] = max(q_value, key=q_value.get)
+        return self.pi[state]
         # ***********
 
     def getAction(self, state):
